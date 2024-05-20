5706267b82347fe43d553a42f5a19d4c45bdc55b
Daniel Sousa <daniel.sousa@msn.com>
2024-05-19 22:11:57 -0300

    feat: adicionado animações

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,30 @@
 import { defineConfig } from 'astro/config';
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
   base: "/",
+  integrations: [
+    swup({
+      theme: "slide",
+      animationClass: "transition-",
+      containers: ["main"],
+      cache: true,
+      preload: true,
+      accessibility: true,
+      forms: false,
+      morph: false,
+      parallel: false,
+      progress: false,
+      routes: false,
+      smoothScrolling: true,
+      updateBodyClass: false,
+      updateHead: true,
+      reloadScripts: true,
+      debug: false,
+      loadOnIdle: true,
+      globalInstance: false,
+    }),
+  ],
 });
