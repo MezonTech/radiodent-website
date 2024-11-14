aa1a754959a5cbddc76ebf72f3b62b7ea0fe5b5e
Daniel Costa <daniel.sousa@msn.com>
2024-11-14 15:23:20 -0300

    Add astro-robots-txt and update dependencies in package.json and config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,33 +2,31 @@ import { defineConfig } from "astro/config";
 import swup from "@swup/astro";
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   // site: "https://github.com/MezonTech",
   site: "http://localhost:4321",
   base: "/radiodent-website",
-  integrations: [
-    swup({
-      theme: "slide",
-      animationClass: "transition-",
-      containers: ["main"],
-      cache: true,
-      preload: true,
-      accessibility: true,
-      forms: false,
-      morph: false,
-      parallel: false,
-      progress: false,
-      routes: false,
-      smoothScrolling: true,
-      updateBodyClass: false,
-      updateHead: true,
-      reloadScripts: true,
-      debug: false,
-      loadOnIdle: true,
-      globalInstance: false,
-    }),
-    sitemap(),
-    robotsTxt(),
-  ],
+  integrations: [robotsTxt(), swup({
+    theme: "slide",
+    animationClass: "transition-",
+    containers: ["main"],
+    cache: true,
+    preload: true,
+    accessibility: true,
+    forms: false,
+    morph: false,
+    parallel: false,
+    progress: false,
+    routes: false,
+    smoothScrolling: true,
+    updateBodyClass: false,
+    updateHead: true,
+    reloadScripts: true,
+    debug: false,
+    loadOnIdle: true,
+    globalInstance: false,
+  }), sitemap()],
 });
